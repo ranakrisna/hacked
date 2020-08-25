5797bca2294c970de4b04341f67dac455f9275e1
ranakrisna <35886521+ranakrisna@users.noreply.github.com>
2020-08-25 15:57:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
 $result = [];
 $result["time"] = time();
 $result["tipe"] = $_SERVER['REQUEST_METHOD'];
-$result["data"] = file_get_contents('php://input');;
+$result["data"] = file_get_contents('php://input');
+$result["header"] = [];
+foreach (getallheaders() as $header => $value) {
+    $result["header"][$header] =  $value;
+}
 echo json_encode($result);
